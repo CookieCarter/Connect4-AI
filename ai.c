1aa86ceb6831a8a198218c9dde50da5223d274d7
CookieCarter <28cweaver@go.dsdmail.net>
2025-08-06 16:58:48 -0600

    Agents now fight in a tourney and have a score.

--- ai.c
+++ ai.c
@@ -15,13 +15,14 @@
 
 #define boardHeight 6
 #define boardWidth 7
-#define games (agents/2)
 
 //network values
 
-#define winners 50 //10
-#define children 9 //1
+#define winners 100
+#define children 4
 #define agents (winners*children+winners) //should be even
+#define games (agents/2)
+#define fightsPerAgent 3
 #define inputs (boardHeight*boardWidth) //board
 #define nodes 32 //sqrt(input layer nodes * output layer nodes) = 17.1464281995...
 #define outputs boardWidth
@@ -33,14 +34,13 @@
 
 struct agent {
     double weights[totalWeights];
+    unsigned int score;
 };
 
 struct game {
     int board[boardHeight][boardWidth];
     int p1;
     int p2;
-    int winner;
-    int loser;
 };
 
 
@@ -81,6 +81,22 @@ void scaledRandArray(double array[], int len) {
         // scale the random number by the sqrt of the number of inputs
         array[i] = randDouble * sqrt(1.0 / numOfInputs);
     }
+}
+
+void shuffleAgents(struct agent array[], int n) {
+    for (int i = n - 1; i > 0; i--) {
+        // Pick a random index from 0 to i
+        int j = randInt(i + 1);
+
+        // Swap element i with element j
+        struct agent temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+int compareAgents(const void *a, const void *b) { //For qsort
+    return (((struct agent *)b)->score - ((struct agent *)a)->score);
 }
 
 //return the index of the highest value in the array
@@ -207,6 +223,7 @@ int main(int argc, char const *argv[]) {
     bool verbose = false, silent = false;
     double percent;
 
+    //Get user values
     if (argc > 3) {if (!(rounds = atoi(argv[1]))) help();} else {help();}
     if ((startPercent = atoi(argv[2]))<0||startPercent>100) help();
     if ((stopPercent = atoi(argv[3]))<0||stopPercent>100) help();
@@ -219,7 +236,7 @@ int main(int argc, char const *argv[]) {
             help();
         }
     }
-
+    
     //setup agents
     struct agent* agentList = malloc(agents * sizeof(struct agent));
     if (!agentList) {
@@ -231,134 +248,111 @@ int main(int argc, char const *argv[]) {
         scaledRandArray(agentList[i].weights, totalWeights);
     }
 
-    int* parents = malloc(winners * sizeof(int));
-    struct agent* tempAgentList = malloc(agents * sizeof(struct agent));
-
     //setup games
-    struct game gameList[games];
-    for (int i = 0; i < games; i++) {
-        for (int j = 0; j < boardHeight; j++) memset(gameList[i].board[j], 0, boardWidth * sizeof(int));
-        gameList[i].p1 = i*2;
-        gameList[i].p2 = i*2+1;
-        gameList[i].winner = -1;
-        gameList[i].loser = -1;
-    }
+    struct game gameState;
 
-    // while(true) { //When to stop?
+    //Start of rounds
     for (int round = 0; round < rounds; round++) {
         if (!verbose&&!silent) displayProgressBar(round, rounds);
-        for (int i = 0; i < games; i++) for (int j = 0; j < boardHeight; j++) for (int k = 0; k < boardWidth; k++) gameList[i].board[j][k] = 0;
         if (verbose) printf("Round %i start\n", round);
-        percent = ((-(double)startPercent/((double)rounds*((double)stopPercent/(double)100)))*(double)round+startPercent);
-        for (int i = 0; i < games; i++) {
-            if (verbose) printf("Game %i start\n", i);
-            while(true) {
-                if (verbose) printf("game round start\n");
-                if (randInt(101) >= percent) {
-                    boardToInput(gameList[i].board, tempInput, 1);
-                    evaluateNetwork(tempInput, agentList[gameList[i].p1].weights, tempOutput);
-                    if (verbose) for (int j = 0; j < outputs; j++) {printf("%f\n",tempOutput[j]);}
-                    tempVar = highestValue(tempOutput, boardWidth, gameList[i].board);
-                } else {
-                    tempVar = randInt(boardWidth);
-                    while (xToY(gameList[i].board, tempVar) == -1) {
-                        tempVar = ++tempVar%boardWidth;
+        percent = ((-(double)startPercent/((double)rounds*((double)stopPercent/(double)100)))*(double)round+startPercent); //Set percentage for being random
+
+        for (int i = 0; i < agents; i++) {
+            agentList[i].score = 0;
+        }
+
+        for (int gameRound = 0; gameRound < fightsPerAgent; gameRound++) {
+            if (verbose) printf("Game round %i start\n", gameRound);
+
+            //shuffle agents for new pairings
+            shuffleAgents(agentList, agents);
+
+            for (int i = 0; i < games; i++) {
+                //Reset game state
+                for (int j = 0; j < boardHeight; j++) memset(gameState.board[j], 0, boardWidth * sizeof(int));
+                gameState.p1 = i*2;
+                gameState.p2 = i*2+1;
+
+                if (verbose) printf("Game %i start\n", i);
+                while(true) { //Run until a win or tie happens
+                    if (verbose) printf("Start of round of game\n");
+
+                    if (fullColumns(gameState.board)==boardWidth) { //Check if all tiles are full (tie)
+                        agentList[gameState.p1].score += 1;
+                        agentList[gameState.p2].score += 1;
+                        break;
                     }
-                }
-                if (tempVar==-1) {if (verbose)printf("Columns Full Error\n");goto fullColumn;}
-                if (verbose) printf("%i\n",tempVar);
-                if (xToY(gameList[i].board, tempVar) == -1) {
-                    printf("Full Column Error");
-                    exit(ERANGE);
-                }
-                gameList[i].board[xToY(gameList[i].board, tempVar)][tempVar] = 1;
-                if (checkWin(gameList[i].board, 1)) {
-                    gameList[i].winner = gameList[i].p1;
-                    gameList[i].loser = gameList[i].p2;
-                    break;
-                }
 
-                if (randInt(101) >= percent) {
-                    boardToInput(gameList[i].board, tempInput, 2);
-                    evaluateNetwork(tempInput, agentList[gameList[i].p2].weights, tempOutput);
-                    if (verbose) for (int j = 0; j < outputs; j++) {printf("%f\n",tempOutput[j]);}
-                    tempVar = highestValue(tempOutput, boardWidth, gameList[i].board);
-                } else {
-                    tempVar = randInt(boardWidth);
-                    while (xToY(gameList[i].board, tempVar) == -1) {
-                        tempVar = ++tempVar%boardWidth;
-                    }
-                }
-                if (tempVar==-1&&verbose) {printf("Columns Full Error\n");goto fullColumn;}
-                if (verbose) printf("%i\n",tempVar);
-                if (xToY(gameList[i].board, tempVar) == -1) {
-                    printf("Full Column Error");
-                    exit(ERANGE);
-                }
-                gameList[i].board[xToY(gameList[i].board, tempVar)][tempVar] = 2;
-                if (checkWin(gameList[i].board, 2)) {
-                    gameList[i].winner = gameList[i].p2;
-                    gameList[i].loser = gameList[i].p1;
-                    break;
-                }
+                    for (int player = 1; player < 2; player++) {
 
-                fullColumn:
-                if (fullColumns(gameList[i].board)==boardWidth) { //random in case of tie
-                    gameList[i].winner = (tempVar = randInt(2))?gameList[i].p1:gameList[i].p2;
-                    gameList[i].loser = (tempVar)?gameList[i].p2:gameList[i].p1;
-                    break;
-                }
-
-                if (verbose) {
-                    for (int j = 0; j < boardHeight; j++) {
-                        for (int k = 0; k < boardWidth; k++) {
-                            printf("%i",gameList[i].board[j][k]);
+                        if (randInt(101) >= percent) { //Decide to be random or not
+                            boardToInput(gameState.board, tempInput, player);
+                            evaluateNetwork(tempInput, agentList[gameState.p1].weights, tempOutput);
+                            if (verbose) for (int j = 0; j < outputs; j++) {printf("%f\n",tempOutput[j]);}
+                            tempVar = highestValue(tempOutput, boardWidth, gameState.board);
+                        } else { //Random
+                            tempVar = randInt(boardWidth);
+                            while (xToY(gameState.board, tempVar) == -1) {
+                                tempVar = ++tempVar%boardWidth;
+                            }
                         }
-                        printf("\n");
+
+                        if (verbose) printf("%i\n",tempVar);
+                        if (xToY(gameState.board, tempVar) == -1) {
+                            printf("Full Column Error");
+                            exit(ERANGE);
+                        }
+
+                        gameState.board[xToY(gameState.board, tempVar)][tempVar] = player;
+                        if (checkWin(gameState.board, player)) {
+                            if (player == 1) {
+                                agentList[gameState.p1].score += 3;
+                            } else {
+                                agentList[gameState.p2].score += 3;
+                            }
+                            break;
+                        }
+
+                    }
+
+                    if (verbose) {
+                        for (int j = 0; j < boardHeight; j++) {
+                            for (int k = 0; k < boardWidth; k++) {
+                                printf("%i",gameState.board[j][k]);
+                            }
+                            printf("\n");
+                        }
                     }
                 }
             }
         }
         if (verbose) printf("Start Sorting\n");
-        
-        // get the winners
-        for (int i = 0; i < winners; i++) {
-            parents[i] = gameList[i].winner;
-        }
 
-        // copy winners to next generation
-        for (int i = 0; i < winners; i++) {
-            memcpy(tempAgentList[i].weights, agentList[parents[i]].weights, totalWeights * sizeof(double));
-        }
+        //sort agents to get winners
+        qsort(agentList, agents, sizeof(struct agent), compareAgents);
 
         // winners reproduce
         for (int i = winners; i < agents; i++) {
             // pick a random winner to be the parent
-            int parentIndex = parents[randInt(winners)];
+            int parentIndex = randInt(winners);
             
             // copy the parent's weights to the child
-            memcpy(tempAgentList[i].weights, agentList[parentIndex].weights, totalWeights * sizeof(double));
+            memcpy(agentList[i].weights, agentList[parentIndex].weights, totalWeights * sizeof(double));
 
             
             for (int j = 0; j < mutations; j++) {
                 int mutationIndex = randInt(totalWeights);
                 
-                // tempAgentList[i].weights[mutationIndex] = randDouble;
+                // agentList[i].weights[mutationIndex] = randDouble;
                 
-                tempAgentList[i].weights[mutationIndex] += randDouble * 0.1;
+                agentList[i].weights[mutationIndex] += randDouble * 0.2;
+
+                if (agentList[i].weights[mutationIndex] > 4.0) {
+                    agentList[i].weights[mutationIndex] = 4.0;
+                } else if (agentList[i].weights[mutationIndex] < -4.0) {
+                    agentList[i].weights[mutationIndex] = -4.0;
+                }
             }
-        }
-
-        // copy next generation to current one.
-        memcpy(agentList, tempAgentList, agents * sizeof(struct agent));
-
-
-        // assign agents to games
-        for (int i = 0; i < games; i++) {
-            gameList[i].p1 = i*2;
-            gameList[i].p2 = i*2+1;
-            gameList[i].winner = -1;
-            gameList[i].loser = -1;
         }
 
         if (verbose) printf("Game Complete\n");
@@ -371,7 +365,7 @@ int main(int argc, char const *argv[]) {
         exit(ENOENT);
     }
     for (int i = 0; i < (totalWeights); i++) {
-        fprintf(fptr, "%f,", agentList[gameList[0].p1].weights[i]);
+        fprintf(fptr, "%f,", agentList[0].weights[i]);
     }
     fclose(fptr);
 
